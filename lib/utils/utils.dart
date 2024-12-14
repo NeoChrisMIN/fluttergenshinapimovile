a974c13a2289da664fb8552ea39166bfd8b1f7e8
NeoChrisMIN <125387759+NeoChrisMIN@users.noreply.github.com>
2024-12-14 14:20:42 +0100

    fix url base

--- lib/utils/utils.dart
+++ lib/utils/utils.dart
@@ -1,2 +1,2 @@
-const urlBaseGlobal = "https://api-genshin-genshin.up.railway.ap";
+const urlBaseGlobal = "https://api-genshin-genshin.up.railway.app";
     //"https://charismatic-fulfillment-production.up.railway.app"; //"http://192.168.100.216:80"; //"http://10.0.2.2:80";
